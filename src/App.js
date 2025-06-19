82367f84cd83f12d3f1a7c112523d61dfd5183f4
ShivamSh002 <shivamsharma98879@gmail.com>
2025-06-19 18:16:23 +0530

    small changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import "./App.css"
-import nftImage from "./asset/image-equilibrium.jpg"; 
-import creator from "./asset/logo.jpeg";
+import React from "react";
+import "./App.css";
+import nftImage from "./asset/image-equilibrium.jpg";
 import clock from "./asset/icon-clock.png";
 import eth from "./asset/icon-ethereum.png";
 import view from "./asset/icon-view.png";
@@ -16,8 +15,10 @@ const App = () => {
             <img src={view} alt="View Icon" />
           </div>
         </div>
-        <h1 className="card__title">Equilibrium #3429</h1>
-        <p id="info" className="description">Our Equilibrium collection promotes balance and calm.</p>
+        <div className="card__title"><h1>Equilibrium #3429</h1></div>
+        <p id="info" className="description">
+          Our Equilibrium collection promotes balance and calm.
+        </p>
         <div className="card__time">
           <div className="card__time-left">
             <img src={eth} alt="ETH Icon" className="time-icon" />
@@ -29,12 +30,33 @@ const App = () => {
           </div>
         </div>
         <div className="card__creator">
-          <img src={creator} alt="Creator Image" className="creator-img" />
-          <p><span className="creator-name">Creation of Jules Wyvern</span></p>
+          <img
+            src="https://media.licdn.com/dms/image/D5603AQHmisevs6V-Wg/profile-displayphoto-shrink_200_200/0/1711565017432?e=2147483647&v=beta&t=2gxdXF049G8KQ53xOdouFjL_DKFruEzC2XH5LmLtem4"
+            alt="Creator Image"
+            className="creator-img"
+          />
+          <p>
+            <span className="creator-name">Creation of Jules Wyvern</span>
+          </p>
         </div>
         <footer className="attribution">
-          Challenge by <a href="https://www.crio.do" target="_blank" rel="noopener noreferrer">Crio.Do</a>. 
-          Coded by <a href="https://www.github.com/ShivamSh002" target="_blank" rel="noopener noreferrer">Shivam Sharma</a>.
+          Challenge by{" "}
+          <a
+            href="https://www.crio.do"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Crio.Do
+          </a>
+          . Coded by{" "}
+          <a
+            href="https://www.github.com/ShivamSh002"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Shivam Sharma
+          </a>
+          .
         </footer>
       </main>
     </div>
